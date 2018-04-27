8369915590d1614f071e7e77e7a875e6c923effc
WISS <hlucca@gmx.net>
2018-04-27 09:36:39 +0200

    Add libraries

---
+++ src/views/MainViewTest.java
@@ -0,0 +1,14 @@
+package views;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainViewTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
